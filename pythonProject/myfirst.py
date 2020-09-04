18acbb23be0669d625666c4b402cab169278ec08
vivek <vivek254258@gmail.com>
2020-09-04 11:30:13 +0530

    forgot email

---
+++ pythonProject/myfirst.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("http://www.gmail.com")
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys("vivekasadsff")
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div[1]/div/div/button/div[2]").click()
+time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[3]/button").click()
+time.sleep(10)
+driver.close()
