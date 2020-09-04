97533761a7ea4f956af01b86c90a5d801db982a8
vivek <vivek254258@gmail.com>
2020-09-04 19:20:49 +0530

    Signed-off-by: vivek <vivek254258@gmail.com>

--- pythonProject/111.py
+++ pythonProject/111.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import time
 driver = webdriver.Chrome()
 driver.get("http://www.google.com")
+
 time.sleep(5)
 driver.find_element_by_xpath("/html/body/div/div[2]/form/div[2]/div[1]/div[1]/div/div[2]/input").send_keys("smiley")
 time.sleep(5)
