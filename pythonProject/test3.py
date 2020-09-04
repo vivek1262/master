145952672f6d8a8f16394f0280c0a68ce2dd3b45
vivek <vivek254258@gmail.com>
2020-09-04 20:39:11 +0530

    print

--- pythonProject/test3.py
+++ pythonProject/test3.py
@@ -1,10 +1,11 @@
-print("Hello")
+from selenium import webdriver
+import time
 
-dldks
-dsdfs
-dfew
-4r34
-try:erger
+driver = webdriver.Chrome()
+driver.get("https://www.geeksforgeeks.org/python-tkinter-tutorial/#geometry")
+driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/article/div/div[2]/ul[1]/li[4]/a").click()
+time.sleep(5)
+driver.forward()
+time.sleep(5)
 
-fs
-from errno
+driver.close()
